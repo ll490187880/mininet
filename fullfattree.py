3725edaebf2de3d37ef0a5918b67f1308ea793e9
ll490187880 <490187880@qq.com>
2017-07-07 17:37:54 +0800

    Update fullfattree.py

--- fullfattree.py
+++ fullfattree.py
@@ -16,24 +16,6 @@ class MyTopo( Topo ):
 
     def __init__( self ):
         "Create custom topo."
-    def simpleTest():
-	"Create and test a simple network"
-	topo = MyTopo()
-	net = Mininet( topo, controller=RemoteController, host=CPULimitedHost, link=TCLink )
-	net.addController( 'c0', controller=RemoteController, ip='20.0.1.3', port=6633 )
-	net.start()
-	print "Dumping host connections"
-	dumpNodeConnections(net.hosts )
-	print "Testing network connectivity"
-	net.pingAll()
-	print "Testing bandwidth between h1 with h2, h3, h5"
-	h1, h2 = net.get( 'h1', 'h2' )
-	net.iperf( ( h1, h2 ) )
-	h1, h3 = net.get( 'h1', 'h3' )
-	net.iperf( ( h1, h3 ) )
-	h1, h5 = net.get( 'h1', 'h5' )
-	net.iperf( ( h1, h5 ) )
-	net.stop()
 
         # Initialize topology
         Topo.__init__( self )
@@ -79,11 +61,29 @@ class MyTopo( Topo ):
                 	host = self.addHost( 'h{}'.format( count ) )
                 	self.addLink( sw1, host )
                 	count += 1
-	if __name__ == '__main__':
-	    # Tellmininet to print useful information
-	    setLogLevel( 'info' )
-	    simpleTest()
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+ def simpleTest():
+	"Create and test a simple network"
+	topo = MyTopo()
+	net = Mininet( topo, controller=RemoteController, host=CPULimitedHost, link=TCLink )
+	net.addController( 'c0', controller=RemoteController, ip='20.0.1.3', port=6633 )
+	net.start()
+	print "Dumping host connections"
+	dumpNodeConnections(net.hosts )
+	print "Testing network connectivity"
+	net.pingAll()
+	print "Testing bandwidth between h1 with h2, h3, h5"
+	h1, h2 = net.get( 'h1', 'h2' )
+	net.iperf( ( h1, h2 ) )
+	h1, h3 = net.get( 'h1', 'h3' )
+	net.iperf( ( h1, h3 ) )
+	h1, h5 = net.get( 'h1', 'h5' )
+	net.iperf( ( h1, h5 ) )
+	net.stop()
+if __name__ == '__main__':
+	# Tellmininet to print useful information
+	setLogLevel( 'info' )
+	simpleTest()
+
 
 
 
